76b877270a07faa5a77ba8e2a8aff436423c5437
ARTHUR F ARAUJO <114027065+Arthur010203@users.noreply.github.com>
2025-03-26 08:14:58 -0300

    Atualizar o MainActivity.java

--- app/src/main/java/com/example/alcoolougasolina/MainActivity.java
+++ app/src/main/java/com/example/alcoolougasolina/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     private TextInputEditText editPrecoAlcool, editPrecoGasolina;
     private TextView textResultado;
 
-    @Override
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
